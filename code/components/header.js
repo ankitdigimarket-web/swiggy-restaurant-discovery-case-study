ecb341ebdce7b1508c4cbce830d18d9017de4b17
ankitdigimarket-web <ankitdigimarket@gmail.com>
2026-03-09 02:00:46 +0530

    Add Header component with styling and content

---
+++ code/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div style={{
+      background:"#0f8a5f",
+      color:"white",
+      padding:"20px"
+    }}>
+      <h3>Work</h3>
+      <p>Sai Shree Boys PG...</p>
+    </div>
+  );
+}
+
+export default Header;
